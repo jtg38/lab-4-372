7144e32ebc8fc87323c6eabab6043d663c79ea18
abriltr <abriltorres@arizona.edu>
2025-03-30 12:49:56 -0700

    Create adc.h

---
+++ adc.h
@@ -0,0 +1,12 @@
+// Author: Abril Torres             
+// Assignment:     Lab 4
+//----------------------------------------------------------------------//
+
+#ifndef ADC_H
+#define ADC_H
+
+#include <avr/io.h>
+
+    void initADC();
+    unsigned int readADC();
+#endif
