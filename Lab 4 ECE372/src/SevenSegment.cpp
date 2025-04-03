d43605b958b1c241b09744a3add048c148c69e55
abriltr <abriltorres@arizona.edu>
2025-04-03 12:41:00 -0700

    Update SevenSegment.cpp

--- Lab 4 ECE372/src/SevenSegment.cpp
+++ Lab 4 ECE372/src/SevenSegment.cpp
@@ -19,16 +19,16 @@ void initSevenSegment() {
 void displayDigit(int digit) {
     // Binary patterns for seven-segment display (0-9)
     static const int segments[11] = {
-        0b00111111,  // Segments for 0
-        0b00000110,  // Segments for 1
-        0b01011011,  // Segments for 2
-        0b01001111,  // Segments for 3
-        0b01100110,  // Segments for 4
-        0b01101101,  // Segments for 5
-        0b01111101,  // Segments for 6
-        0b00000111,  // Segments for 7
+        0b01111110,  // Segments for 0
+        0b01100000,  // Segments for 1
+        0b00111101,  // Segments for 2
+        0b01110101,  // Segments for 3
+        0b01100011,  // Segments for 4
+        0b01010111,  // Segments for 5
+        0b01011111,  // Segments for 6
+        0b01110000,  // Segments for 7
         0b01111111,  // Segments for 8
-        0b01101111,  // Segments for 9
+        0b01110111,  // Segments for 9
         0b00000000   // Segments for blank (off)
     };
     PORTC = segments[digit];
