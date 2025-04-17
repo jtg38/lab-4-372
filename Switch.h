c7a1d4ea06c79b5487e38b3182fd290179b2590e
tallenmon26 <tmonnett@arizona.edu>
2025-04-17 14:11:38 -0700

    Create Switch.h

---
+++ Switch.h
@@ -0,0 +1,13 @@
+// Author: Tallen Monnett        
+// Net ID: tmonnett        
+// Date: 3/29/2025         
+// Assignment: Lab 4
+// Description: Function prototypes for switch.cpp
+//----------------------------------------------------------------------//
+
+#ifndef SWITCH_H
+#define SWITCH_H
+
+void initSwitch();
+
+#endif
