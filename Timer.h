dfd92c1c30b38e21c2525af032f2d1572e50b21a
tallenmon26 <tmonnett@arizona.edu>
2025-04-17 14:12:43 -0700

    Create Timer.h

---
+++ Timer.h
@@ -0,0 +1,15 @@
+// Author: Tallen Monnett        
+// Net ID: tmonnett        
+// Date: 3/27/2025         
+// Assignment: Lab 4
+//----------------------------------------------------------------------//
+
+#ifndef TIMER_H
+#define TIMER_H
+
+#include <avr/io.h>
+
+void initTimer1();
+void delayMs(unsigned int delay); 
+
+#endif
