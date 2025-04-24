08929dbc444a90c03a46e8b62c26e91cd1e1ea8e
jtg38 <jtg3@arizona.edu>
2025-04-24 11:18:17 -0700

    Update Timer.cpp

--- Timer.cpp
+++ Timer.cpp
@@ -9,14 +9,15 @@
 void initTimer1() {
     // Clear all WGM bits first
     TCCR1A &= ~((1 << WGM10) | (1 << WGM11));
-    TCCR1B &= ~(1 << WGM12);
+    TCCR1B |= (1 << WGM12);
     // Set WGM01 for CTC mode
-    TCCR1A |= (1 << WGM12);
+    TCCR1B &= ~(1 << WGM13);
 }
 
 void delayMs(unsigned int delay) {
     // Configure for 1ms intervals with 64 prescaler
     TCCR1B = (1 << CS11) | (1 << CS10); 
+    TCCR1B &= ~(1 << CS12);
     OCR1A = 249;
 
     for(unsigned int i = 0; i < delay; i++) {
@@ -28,6 +29,7 @@ void delayMs(unsigned int delay) {
         while(!(TIFR1 & (1 << OCF1A)));
     }
     
+    TCCR1B &= ~(1 << CS11) | (1 << CS10); 
     // Stop timer by clearing prescaler
     TCCR1B = 0;
 }
