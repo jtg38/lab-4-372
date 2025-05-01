e85b288eb5b23749e5ed96d67f1339478facbebc
abriltr <abriltorres@arizona.edu>
2025-05-01 13:33:10 -0700

    Create pwm.h

---
+++ pwm.h
@@ -0,0 +1,13 @@
+// Author: Abril Torres            
+// Assignment:     Final Project 
+//----------------------------------------------------------------------//
+
+#ifndef PWM_H
+#define PWM_H
+
+void initPWMTimer3();
+void initMotorDirections();
+void setDirectionForward();
+void stopMotors();
+
+#endif
