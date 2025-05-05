50dcc5eb8c05baafeaf2896a17c65c82d51c7c6e
abriltr <abriltorres@arizona.edu>
2025-05-05 11:08:25 -0700

    Update pwm.h

--- pwm.h
+++ pwm.h
@@ -6,8 +6,12 @@
 #define PWM_H
 
 void initPWMTimer3();
+void initPWMTimer4();
 void initMotorDirections();
 void setDirectionForward();
 void stopMotors();
+void setDirectionBackward();
+void setDirectionLeft();
+void setDirectionRight();
 
 #endif
