bea90b4e362417400cac461e376edb902fe4f84e
abriltr <abriltorres@arizona.edu>
2025-05-05 11:57:09 -0700

    Create vacuum.h

---
+++ vacuum.h
@@ -0,0 +1,16 @@
+// Author: Abril Torres            
+// Assignment:    Final Project
+//----------------------------------------------------------------------//
+
+#ifndef VACUUM_H
+#define VACUUM_H
+
+void initPWMTimer5();
+void initMotorVacuumDirections();
+void setVacuumDirectionForward();
+void setVacuumDirectionBackward();
+void stopVacuumMotor();
+
+
+
+#endif
