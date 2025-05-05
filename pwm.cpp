880ea80c795cfbbe7a8fb9dda71f127265a203f5
abriltr <abriltorres@arizona.edu>
2025-05-05 11:08:06 -0700

    Update pwm.cpp

--- pwm.cpp
+++ pwm.cpp
@@ -8,39 +8,55 @@
 
 void initPWMTimer3(){ 
     //Fast mode 15 - 1111
-    //Motor A 
+    //Motor A right 
     DDRB |= (1<<PB0) | (1<<PB1);  // IN1, IN2 // Pin B0 is output - OC3A, Pin B1 is output - OC3A
 
-    //Motor B 
-    DDRB |= (1<<PB2) | (1<<PB3);   // IN3, IN4
-
     //PWM control 
-    DDRE |= (1 << PE4) | (1 << PE5); //Pin E4-A and E5-B is output - OC3B 
+    DDRE |= (1 << PE3); //Pin E4-A is output - OC3A 
 
-    // Timer 3 - Fast mode 15 - 1111  
+    // Timer 3 - Fast mode 10 - 111 
     TCCR3A |= (1<<WGM30); 
     TCCR3A |= (1<<WGM31);  
     TCCR3B |= (1<<WGM32);  
-    TCCR3B |= (1<<WGM33);
 
     // Need non-inverted PWM output so 01 for Compare Output Mode, Fast PWM 
     // Still working on timer 3 
-    TCCR3A |= (1 << COM3B1);  // Right motor on PE4
-    TCCR3A |= (1 << COM3C1);  // Left motor on PE5
-
+    TCCR3A |= (1 << COM3A1);  // Right motor on PE4
+ 
     // Prescaler = 1 100 
     TCCR3B |= (1<<CS30);
     TCCR3B &= ~(1<<CS31); 
     TCCR3B &= ~(1<<CS32);
 
-     // Set TOP value for 400 Hz frequency
-     ICR3 = 39999;
+     // Set initial duty cycle to 
+     OCR3A = 700;  // Right motor
+     
+}
+
+    void initPWMTimer4(){
+        //Motor B left 
+    DDRB |= (1<<PB2) | (1<<PB3);   // IN3, IN4
+
+    DDRH |= (1 << PH3); //Pin H4 is output - OC4A
+
+    // Timer 4 - Fast mode 10 - 111
+    TCCR4A |= (1<<WGM40); 
+    TCCR4A |= (1<<WGM41);  
+    TCCR4B |= (1<<WGM42);  
+
+    // Need non-inverted PWM output so 01 for Compare Output Mode, Fast PWM 
+    TCCR4A |= (1 << COM4A1);  // Right motor on PE4
+
+    // Prescaler = 1 100 
+    TCCR4B |= (1<<CS40);
+    TCCR4B &= ~(1<<CS41); 
+    TCCR4B &= ~(1<<CS42);
 
      // Set initial duty cycle to 50%
-     OCR3B = 20000;  // Right motor
-     OCR3C = 20000;  // Left motor379
+     OCR4A = 700;  // left motor
 
-}
+    } 
+
 
     // Initialize Direction Pins on PA0–PA3 to outputs 
     void initMotorDirections() {
@@ -57,13 +73,44 @@ void initPWMTimer3(){
     PORTA &= ~(1 << 1);
 
     // Right motor forward (PA2 HIGH, PA3 LOW)
-    PORTA |= (1 << 2);
-    PORTA &= ~(1 << 3);
+    PORTA &= ~(1 << 2);
+    PORTA |= (1 << 3);
+}
+
+void setDirectionBackward() {
+    // Left motor backward (IN1=LOW, IN2=HIGH)
+    PORTA &= ~(1 << PA0);
+    PORTA |= (1 << PA1);
+
+    // Right motor backward (IN3=HIGH, IN4=LOW)
+    PORTA |= (1 << PA2);
+    PORTA &= ~(1 << PA3);
+}
+
+void setDirectionLeft() {
+    // Left motor backward
+    PORTA &= ~(1 << PA0);
+    PORTA |= (1 << PA1);
+
+    // Right motor forward
+    PORTA &= ~(1 << PA2);
+    PORTA |= (1 << PA3);
+}
+
+void setDirectionRight() {
+    // Left motor forward
+    PORTA |= (1 << PA0);
+    PORTA &= ~(1 << PA1);
+
+    // Right motor backward
+    PORTA |= (1 << PA2);
+    PORTA &= ~(1 << PA3);
 }
 
 void stopMotors() {
     OCR3B = 0;
     OCR3C = 0;
+    PORTA &= ~((1 << 0) | (1 << 1) | (1 << 2) | (1 << 3)); // Set all direction pins to low
 }
 
 
